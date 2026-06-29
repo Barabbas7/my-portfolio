091379bf074c2042a4c0e4253c94296f782dbe53
Daniel Kebede <barabbas218@gmail.com>
2026-06-29 10:27:33 +0300

    Scroll effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import SmoothScrollbar from "@/components/ui/SmoothScrollProvider";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
@@ -41,7 +42,9 @@ export default function RootLayout({
       lang="en"
       className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <SmoothScrollbar>{children}</SmoothScrollbar>
+      </body>
     </html>
   );
 }
