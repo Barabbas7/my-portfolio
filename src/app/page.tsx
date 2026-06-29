b7541ffa162650e7cc6a931de84028a1593fc74e
Daniel Kebede <barabbas218@gmail.com>
2026-06-29 09:09:00 +0300

    Initial Project Section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
+import Projects from "@/components/sections/Projects";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
+      <Projects />
     </main>
   );
 }
